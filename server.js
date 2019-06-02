5c312c6569592b18ada071962c51dc160a2e8730
Hannah Vaughan <vaughanh@oregonstate.edu>
2019-06-01 20:08:28 -0700

    Add handlebars

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 var path = require('path');
 var express = require('express');
+var exphbs = require('express-handlebars');
 var mysql = require('mysql');
 var app = express();
 var port = process.env.PORT || 3000;
-
-app.use(express.static('public'));
 
 var con = mysql.createConnection({
     host: "classmysql.engr.oregonstate.edu",
@@ -19,8 +18,18 @@ con.connect(function (err) {
     console.log("Connected to database.");
 });
 
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+    res.status(200).render('index');
+});
+
 app.get('*', function (req, res) {
-    res.status(404).sendFile(path.join(__dirname,'public', '404.html'))
+    // res.status(404).sendFile(path.join(__dirname,'public', '404.html'))
+    res.status(404).render('404');
 });
 
 app.listen(port, function() {
