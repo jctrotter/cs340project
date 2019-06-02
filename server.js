38e149dc0e3e35cbbac45b7ddd091e8179e29df5
Hannah Vaughan <vaughanh@oregonstate.edu>
2019-06-01 19:32:28 -0700

    Add connection to MySQL DB with npm

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 var path = require('path');
 var express = require('express');
+var mysql = require('mysql');
 var app = express();
 var port = process.env.PORT || 3000;
 
 app.use(express.static('public'));
+
+var con = mysql.createConnection({
+    host: "classmysql.engr.oregonstate.edu",
+    user: "cs340_vaughanh",
+    password: "2189"
+});
+
+con.connect(function (err) {
+    if (err) {
+        throw err;
+    }
+    console.log("Connected to database.");
+});
 
 app.get('*', function (req, res) {
     res.status(404).sendFile(path.join(__dirname,'public', '404.html'))
