e2c12f9bb17d0985e915962b440109aa6c442f01
Hannah Vaughan <vaughanh@oregonstate.edu>
2019-06-04 20:19:32 -0700

    Remove connect function call from config file

--- public/js/sql_db_config.js
+++ public/js/sql_db_config.js
@@ -1,16 +1,9 @@
-var mysql = require('msql');
+var mysql = require('mysql');
 
 var con = mysql.createConnection({
     host: "classmysql.engr.oregonstate.edu",
     user: "cs340_vaughanh",
     password: "2189"
-});
-
-con.connect(function (err) {
-    if (err) {
-        throw err;
-    }
-    console.log("Connected to database.");
 });
 
 module.exports = con;
