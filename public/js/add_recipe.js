7f94d0b0cc68b5659ea831e23745ed983dd9d8b7
jctrotter <trotterj@oregonstate.edu>
2019-06-06 16:10:39 -0700

    working on add_recipe

---
+++ public/js/add_recipe.js
@@ -0,0 +1,31 @@
+import { strict } from "assert";
+import { stringify } from "querystring";
+
+function getRecipeToAdd(event) {
+	// get our values
+    var recipe_name = $('#recipe-name').val();
+    var recipe_URL = $('#photo-url').val();
+
+    // increase ingredients and steps when respective adds clicked
+
+    var amts = [];
+    var ingredients = [];
+    var steps = [];
+
+    $('#new-ingredients').children('#amt').each(function () {
+        amts.append(this.val())
+    });
+
+    $('#new-ingredients').children('#ingredient').each(function () {
+        ingredients.append(this.val())
+    });
+
+    $('#new-steps').children('#step').each(function () {
+        steps.append(this.val())
+    });
+
+}
+
+$(document).ready(() => {
+	$('#recipe-submit').click(getRecipeToAdd);
+});
